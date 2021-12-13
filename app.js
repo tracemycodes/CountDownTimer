e9e8de95cadfcbcb88e9c9b5f09588b8795894bd
Agba Emmanuel <agbaayoh@gmail.com>
2021-12-13 02:15:08 -0700

    countdown functionality made effective

--- app.js
+++ app.js
@@ -3,43 +3,8 @@ const UIhour = document.querySelectorAll(".count-hours");
 const UImin = document.querySelectorAll(".count-min");
 const UIsec = document.querySelectorAll(".count-sec");
 
-// const countTimeObj = {
-//   days: 43,
-//   hours: 18,
-//   minutes: 23,
-//   seconds: 59,
-// };
 
-// var editedDeadline = setDeadline({
-//   days: Number(formData.get("day")),
-//   hours: Number(formData.get("hour")),
-//   minutes: Number(formData.get("minute")),
-//   seconds: Number(formData.get("second")) + 2,
-// });
-
-// const countTimeObj = CountTime({
-//   days: 43,
-//   hours: 18,
-//   minutes: 23,
-//   seconds: 59,
-// })
-
-// function CountTime (countTimeObj) {
-//   console.log(countTimeObj);
-// };
-
-// const CountTime = (countTimeObj) => {
-//   console.log(countTimeObj);
-// }
-
-const countTimeObj = CountTime({
-  days: Number(43),
-  hours: Number(18),
-  minutes: Number(23),
-  seconds: Number(52),
-});
-
-function CountTime(time) {
+const countTime = (time) => {
   let days = time.days,
     hours = time.hours,
     minutes = time.minutes,
@@ -56,7 +21,14 @@ function CountTime(time) {
   return currentTime;
 }
 
-function calculateCount() {
+const countTimeObj = countTime({
+  days: Number(49),
+  hours: Number(13),
+  minutes: Number(28),
+  seconds: Number(15) + 2,
+});
+
+const calculateCount = () => {
   const countDate = Date.parse(countTimeObj) - new Date().getTime(),
     days = Math.floor(countDate / (1000 * 60 * 60 * 24));
   (hours = Math.floor((countDate / (1000 * 60 * 60)) % 24)),
@@ -72,36 +44,74 @@ function calculateCount() {
   };
 }
 
-let huy = calculateCount();
-console.log(huy);
-
-// setInterval(displayCount, 1000)
-
-function displayCount() {
-  const UIday = document.querySelectorAll(".count-day");
-  const UIhour = document.querySelectorAll(".count-hours");
-  const UImin = document.querySelectorAll(".count-min");
-  const UIsec = document.querySelectorAll(".count-sec");
-
+const displayCount = () => {
+  let animateDay = document.getElementById("animate-day");
+  let animateHour = document.getElementById("animate-hour");
+  let animateMin = document.getElementById("animate-min");
+  let animateSec = document.getElementById("animate-sec");
   let timeObj = calculateCount();
 
-  console.log(timeObj);
+  UIday.forEach((dayNum) => {
+    let displayDay;
+    if (timeObj.days < 10) {
+      displayDay = "0" + String(timeObj.days);
+    } else {
+      displayDay = String(timeObj.days);
+    }
+    animate(dayNum.textContent, displayDay, animateDay);
+    dayNum.textContent = displayDay;
+  });
 
-  UIday.forEach((dayNum) => (dayNum.textContent = timeObj.days));
-  UIhour.forEach((hourNum) => (hourNum.textContent = timeObj.hours));
-  UImin.forEach(minNum => minNum.textContent = timeObj.minutes);
-  UIsec.forEach(secNum => secNum.textContent = timeObj.seconds)
-  calculateCount()
+
+  UIhour.forEach((hourNum) => {
+    let displayHour;
+    if (timeObj.hours < 10) {
+      displayHour = "0" + String(timeObj.hours);
+    } else {
+      displayHour = String(timeObj.hours);
+    }
+    animate(hourNum.textContent, displayHour, animateHour);
+    hourNum.textContent = displayHour;
+  });
+
+
+  UImin.forEach((minNum) => {
+    let displayMin;
+    if (timeObj.minutes < 10) {
+      displayMin = "0" + String(timeObj.minutes);
+    } else {
+      displayMin = String(timeObj.minutes);
+    }
+    animate(minNum.textContent, displayMin, animateMin);
+    minNum.textContent = displayMin;
+  });
+
+
+  UIsec.forEach((secNum) => {
+    let displaySec;
+    if (timeObj.seconds < 10) {
+      displaySec = "0" + String(timeObj.seconds);
+      // animate()
+    } else {
+      displaySec = String(timeObj.seconds);
+    }
+    animate(secNum.textContent, displaySec, animateSec);
+    secNum.textContent = displaySec;
+  });
 }
 
-displayCount();
-
-setInterval(dothis, 2000)
-
-function dothis () {
-  calculateCount()
-  displayCount()
+const animate = (number, time, elem) => {
+  if (number != time) {
+    setTimeout((stopAnimation) => {
+      elem.classList.add("animate-flip");
+    }, 100);
+    elem.classList.remove("animate-flip");
+  }
 }
-// const deadlineDate = new Date(countTimeObj);
-// console.log(countTimeObj.getTime());
-// console.log(deadlineDate);
+
+setInterval(dothis, 1000);
+
+function dothis() {
+  calculateCount();
+  displayCount();
+}
